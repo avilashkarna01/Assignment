47905396553f5e826283807a0346b1910dfee889
Abhilash karna <avilashkarna1@gmail.com>
2018-06-12 15:48:39 +0530

    initial to be changes

---
+++ assign1.py
@@ -0,0 +1,35 @@
+print("question1")
+print("Hi this is me" )
+print("myself abhilash karna")
+print("\n")
+
+print("question2")
+x="Acad"
+y="View"
+print(x+y)
+print("\n")
+
+print("question3")
+x=input("enter any value:")
+y=input("enter any value:")
+z=input("enter any value:")
+print("the values of x,y,z ")
+print(x,y,z)
+print("\n")
+
+print("question4")
+print("let's get started")
+print("\n")
+
+print("question5")
+s="Acadview"
+course="python"
+fees=6000
+print("%s,%s,%d"%(s,course,fees))
+print("\n")
+
+print("question6")
+name="Tony stark"
+salary=100000
+print("%s,%d"%(name,salary))
+
